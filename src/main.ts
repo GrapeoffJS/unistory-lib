53b816f89b582c6b86107d7e90a006395d81877a
Dmitriy Grape <idmitriy.git@gmail.com>
2022-06-18 01:47:29 +0300

    Enabled CORS, Helmet, API Versioning and API Documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
@@ -9,6 +11,15 @@ async function bootstrap() {
 
     const configService = app.get<ConfigService>(ConfigService);
 
+    app.enableCors({
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+        allowedHeaders: configService
+            .get<string>('CORS_ALLOWED_HEADERS')
+            .split(' '),
+        optionsSuccessStatus: 200,
+        origin: configService.get<string>('CORS_ALLOWED_ORIGINS').split(' '),
+    });
+    app.use(helmet());
     app.useGlobalPipes(
         new ValidationPipe({
             always: true,
@@ -18,6 +29,24 @@ async function bootstrap() {
             forbidNonWhitelisted: true,
         }),
     );
+
+    app.enableVersioning({
+        type: VersioningType.URI,
+        prefix: 'api',
+        defaultVersion: '1',
+    });
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Library API')
+        .setDescription(
+            'Implementation of the API for the library as part of the test task',
+        )
+        .setVersion('1.0')
+        .addTag('Library')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api/docs', app, document);
 
     await app.listen(configService.get<string>('PORT'));
 }
