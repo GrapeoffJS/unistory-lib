a838f6cec32ba8fd2cdce172ebeeff4d9b0964d6
Dmitriy Grape <idmitriy.git@gmail.com>
2022-06-18 14:50:19 +0300

    Controller for readers crud operations

---
+++ src/readers/crud/crud.controller.ts
@@ -0,0 +1,60 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+} from '@nestjs/common';
+import { ApiBody, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+import { UuidDto } from '../../common/dto/uuid.dto';
+import { ReaderEntity } from '../entities/reader.entity';
+import { CrudService } from './crud.service';
+import { CreateReaderDto } from './dto/create-reader.dto';
+import { UpdateReaderDto } from './dto/update-reader.dto';
+
+@ApiTags('Readers')
+@Controller('readers')
+export class CrudController {
+    constructor(private readonly crudService: CrudService) {}
+
+    @ApiResponse({ type: () => ReaderEntity, isArray: true })
+    @Get()
+    async get() {
+        return await this.crudService.get();
+    }
+
+    @ApiParam({ name: 'id' })
+    @ApiResponse({ type: () => ReaderEntity })
+    @Get(':id')
+    async getById(@Param() { id }: UuidDto) {
+        return await this.crudService.getById(id);
+    }
+
+    @ApiBody({ type: () => CreateReaderDto })
+    @ApiResponse({ type: () => ReaderEntity })
+    @Post()
+    async create(@Body() createReaderDto: CreateReaderDto) {
+        return await this.crudService.create(createReaderDto);
+    }
+
+    @ApiParam({ name: 'id' })
+    @ApiBody({ type: () => UpdateReaderDto })
+    @ApiResponse({ type: () => ReaderEntity })
+    @Patch(':id')
+    async update(
+        @Param() { id }: UuidDto,
+        @Body() updateReaderDto: UpdateReaderDto,
+    ) {
+        return await this.crudService.update(id, updateReaderDto);
+    }
+
+    @ApiParam({ name: 'id' })
+    @ApiResponse({ type: () => ReaderEntity })
+    @Delete(':id')
+    async delete(@Param() { id }: UuidDto) {
+        return await this.crudService.delete(id);
+    }
+}
