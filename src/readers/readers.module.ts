2e45f563c2688bb7ca5047eb6a5cce6cc22486cd
Dmitriy Grape <idmitriy.git@gmail.com>
2022-06-18 18:04:40 +0300

    Readers Module

--- src/readers/readers.module.ts
+++ src/readers/readers.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { BooksManagementModule } from './books-management/books-management.module';
 import { CrudModule } from './crud/crud.module';
+import { SubscriptionManagementModule } from './subscription-management/subscription-management.module';
 
 @Module({
-    imports: [CrudModule],
+    imports: [CrudModule, BooksManagementModule, SubscriptionManagementModule],
+    providers: [],
+    controllers: [],
 })
 export class ReadersModule {}
