8697c439b73a8150d24792dbc087cedc161d6315
Dmitriy Grape <idmitriy.git@gmail.com>
2022-06-18 18:25:34 +0300

    POSTGRES_PORT is optional

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { ReadersModule } from './readers/readers.module';
             validationSchema: Joi.object({
                 PORT: Joi.number().required(),
                 POSTGRES_HOST: Joi.string().required(),
-                POSTGRES_PORT: Joi.number(),
+                POSTGRES_PORT: Joi.number().optional(),
                 POSTGRES_USERNAME: Joi.string().required(),
                 POSTGRES_PASSWORD: Joi.string().required(),
                 POSTGRES_DATABASE: Joi.string().required(),
