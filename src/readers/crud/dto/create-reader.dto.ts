4fac8b40eab4d5c6808b270d9905cae5b7609ef7
Dmitriy Grape <idmitriy.git@gmail.com>
2022-06-18 18:47:29 +0300

    Set subscription status on create

--- src/readers/crud/dto/create-reader.dto.ts
+++ src/readers/crud/dto/create-reader.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateReaderDto {
     @ApiProperty()
@@ -11,4 +11,9 @@ export class CreateReaderDto {
     @IsNotEmpty()
     @IsString()
     last_name: string;
+
+    @ApiProperty()
+    @IsOptional()
+    @IsBoolean()
+    has_subscription: boolean;
 }
