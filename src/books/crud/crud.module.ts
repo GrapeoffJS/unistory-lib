2a0d35d3f1bd940dd892cc54fefca55e383860c5
Dmitriy Grape <idmitriy.git@gmail.com>
2022-06-18 15:17:37 +0300

    Imported book entity

--- src/books/crud/crud.module.ts
+++ src/books/crud/crud.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { BookEntity } from '../entities/book.entity';
 import { CrudController } from './crud.controller';
 import { CrudService } from './crud.service';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([BookEntity])],
     providers: [CrudService],
     controllers: [CrudController],
 })
